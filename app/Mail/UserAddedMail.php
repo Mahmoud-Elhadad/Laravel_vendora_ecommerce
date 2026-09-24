cc9bfcc765b4ce82f7cb30fff945baec99724eed
Mahmoud Elhadad <mahmoudelhadad2005@gmail.com>
2026-09-24 19:12:44 +0300

    updated UserAddedMail content

--- app/Mail/UserAddedMail.php
+++ app/Mail/UserAddedMail.php
@@ -29,7 +29,7 @@ class UserAddedMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'User Added Mail',
+            subject: "welcome" .  $this->user->name,
         );
     }
 
