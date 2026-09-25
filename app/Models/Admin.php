0ec2978b8dc918604e19ba20a5c1c77321a7d911
Mahmoud Elhadad <mahmoudelhadad2005@gmail.com>
2026-09-25 20:09:35 +0300

    Modified Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
+    use Notifiable;
+    
     protected $fillable = [
         'name', 'email', 'password', 'phone', 'role', 'gender', 'location', 'age', 'img',
     ];
